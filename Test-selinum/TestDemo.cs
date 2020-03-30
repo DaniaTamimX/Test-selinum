4de7747f55b91de55b9c2128e1488b29c5e866a5
Dania Tamim <dania.tamim@xlent.se>
2020-03-30 14:56:01 +0200

    Add project files.

---
+++ Test-selinum/TestDemo.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_selinum
+{
+    class TestDemo
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver("C:\\Program Files (x86)\\Google\\Chrome\\Application");
+        }
+        [Test]
+        public void Test()
+        {
+            driver.Url = "https://www.google.se";
+        }
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
